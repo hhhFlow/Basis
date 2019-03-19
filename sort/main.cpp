e8a7ffe602dafae324d19f891222aad41383508f
wantaoGong <wantaoGong@gmail.com>
2019-03-19 23:19:48 +0800

    add bubble sort.

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "insert_sort.h"
-
+#include "bubble_sort.h"
 
 
 int main(){
 
-    int a[] = {5,1,2,8,99,1,45,34,66};
+    int a[] = {654,17,39,21,5,1,2,8,99,1,45,34,66};
 
-    insert_sort(a,9);
+    bubble_sort(a,13);
 
-    for(int i = 0; i<9;++i){
+    for(int i = 0; i<13;++i){
         std::cout<<" "<<a[i];
     }
 
